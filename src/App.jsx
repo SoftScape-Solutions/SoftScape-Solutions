e9eadc8e857180ca41e17e787c36a4c5e07f7de2
OmerAqeel <omrraqeel@gmail.com>
2025-10-06 18:29:09 +0100

    created react-app and simple landing page

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LandingPage from './compo/landingPage'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-slate-50 to-blue-50">
+      <LandingPage />
+    </div>
+  )
+}
+
+export default App
