d4df4b236d2bdbf0245a1ae7232a44314fa10cce
SAADhusnain <98428111+SAADhusnain@users.noreply.github.com>
2025-10-06 09:16:17 +0300

    Created a Modular File structure
    Created a Modular File structure

--- softscape-solutions/libs/common/src/index.ts
+++
@@ -1,4 +0,0 @@
-// This file exports common utilities and types that can be used across different applications and libraries.
-
-export * from './utils';
-export * from './types';
--- softscape-solutions/libs/shared-ui/src/index.ts
+++
@@ -1,6 +0,0 @@
-// This file exports shared UI components that can be used across different applications.
-
-export * from './components/Button';
-export * from './components/Card';
-export * from './components/Modal';
-// Add more exports as needed for shared UI components
